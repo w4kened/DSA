300e82bbba52ca1318eedc3b495994c5c9210ffa
w4kened <w4kened@mailfence.com>
2024-11-11 14:19:19 +0100

    bubble sort and merge sort algorithms implemention

---
+++ src/BubbleSort_DSA.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+public class BubbleSort_DSA {
+    public static int[] bubbleSort(int input[]) {
+        boolean swapping = true;
+
+        while (swapping) {
+            swapping = false;
+            for (int i = 0; i < input.length - 1; i++) {
+                if (input[i] > input[i + 1]) {
+                    System.out.println(String.format("Swapping pair %d, %d", input[i], input[i+1]));
+                    swap(input, i, i + 1);
+
+                    System.out.println(Arrays.toString(input));
+                    swapping = true;
+                }
+            }
+
+        }
+
+        return input;
+    }
+
+    private static int[] swap(int arr[], int indexOne, int indexTwo) {
+        int temp = arr[indexOne];
+        arr[indexOne] = arr[indexTwo];
+        arr[indexTwo] = temp;
+
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        bubbleSort(new int[] {1,5,4,6,2,3});
+    }
+}
---
+++ src/MergeSort_DSA.java
@@ -0,0 +1,65 @@
+import java.util.Arrays;
+
+public class MergeSort_DSA {
+    public int[] divide(int[] arr) {
+        int length = arr.length;
+        if (length <= 1) {
+            return arr;
+        }
+        int mid = Math.floorDiv(length, 2);
+        int[] leftArray = Arrays.copyOfRange(arr, 0, mid);
+        int[] rightArray = Arrays.copyOfRange(arr, mid, length);
+        System.out.println("divided left= "+Arrays.toString(leftArray));
+        System.out.println("divided right= "+Arrays.toString(rightArray));
+
+        int[] lDividedArray = divide(leftArray);
+        int[] rDividedArray = divide(rightArray);
+
+        return mergeAndSort(lDividedArray, rDividedArray);
+    }
+
+    public int[] mergeAndSort(int[] left, int[] right) {
+        int[] merged = new int[left.length + right.length];
+        int leftPos = 0;
+        int rightPos = 0;
+        int curIndex = 0;
+
+        while (left.length > leftPos &&
+                right.length > rightPos) {
+            System.out.println("left comparing "+Arrays.toString(left));
+            System.out.println("right comparing "+Arrays.toString(right));
+
+            if (left[leftPos] < right[rightPos]) {
+                merged[curIndex++] = left[leftPos++];
+                System.out.println("from left to merge "+Arrays.toString(merged));
+            }
+            else {
+                merged[curIndex++] = right[rightPos++];
+                System.out.println("from right to merge "+Arrays.toString(merged));
+            }
+
+            continue;
+        }
+
+        System.arraycopy(left, leftPos, merged, curIndex,
+                left.length - leftPos);
+        System.out.println("arraycopy from remaining left to merged "+Arrays.toString(merged));
+
+        System.arraycopy(right, rightPos, merged, curIndex,
+                right.length - rightPos);
+        System.out.println("arraycopy from remaining right to merged "+Arrays.toString(merged));
+
+        return merged;
+
+
+    }
+
+
+    public static void main (String[] args) {
+        int[] inputArr = {3, 5, 2, 90, 4, 7};
+        MergeSort_DSA sorter = new MergeSort_DSA();
+        System.out.println("input "+ Arrays.toString(inputArr));
+
+        System.out.println(Arrays.toString(sorter.divide(inputArr)));
+    }
+}
